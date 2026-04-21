820ec45ae8a57af4ffa5a2f0e4db8cf86c286ce9
kingsleychino <kingsleychinono@gmail.com>
2026-04-21 04:51:46 +0100

    created terraform

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR Range"
+  type        = string
+}
+
+
+variable "subnet_cidr" {
+  description = "Subnet CIDR"
+  type        = list(string)
+}
+
+variable "subnet_names" {
+  description = "Subnet names"
+  type        = list(string)
+  default     = ["Public-Subnet-1", "Public-Subnet-2"]
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "kinsley-bucket-0504"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
